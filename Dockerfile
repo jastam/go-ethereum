2900c92e0a5dbcab13a05786de3c3fc346b3d567
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-06-13 13:34:55 +0200

    Added config for running node

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,10 @@ FROM alpine:latest
 RUN apk add --no-cache ca-certificates
 COPY --from=builder /go-ethereum/build/bin/geth /usr/local/bin/
 
+ADD ./genesis.json.template /genesis.json.template
+ADD ./UTC--2018-06-13T08-21-07.573900500Z--3837a575ea779373777f682b26cbb32d3a00d8cb /UTC--2018-06-13T08-21-07.573900500Z--3837a575ea779373777f682b26cbb32d3a00d8cb
+ADD ./accountpswd.txt /accountpswd.txt
+ADD ./run.sh /run.sh
+
 EXPOSE 8545 8546 30303 30303/udp
-ENTRYPOINT ["geth"]
+#ENTRYPOINT ["geth"]
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -u
+set -e
+
+if [ ! -e /geth.sh ]
+then
+  echo "Creating account"
+  pubkey="3837a575ea779373777f682b26cbb32d3a00d8cb"
+  mkdir /datadir
+  mkdir /datadir/keystore
+  mv /UTC--2018-06-13T08-21-07.573900500Z--${pubkey} /datadir/keystore/UTC--2018-06-13T08-21-07.573900500Z--${pubkey}
+
+  echo "Writing genesis.json"
+  sed -e s/##address##/${pubkey}/g /genesis.json.template > /genesis.json
+
+  echo "Initializing new blockchain"
+  geth --datadir /datadir init /genesis.json
+
+  echo "Writing start script"
+  #echo "geth --datadir /datadir --nousb --networkid 2814 --syncmode 'full' --gcmode 'archive' --identity '${HOSTNAME}' --ws --wsapi 'admin,debug,eth,miner,net,personal,shh,txpool,web3' --wsorigins '*' --ipcdisable --mine --minerthreads 1 --targetgaslimit 0 --gasprice '1' --unlock '0x${pubkey}' --password /accountpswd.txt" > /geth.sh
+  echo "geth --datadir /datadir --nousb --networkid 2814 --syncmode 'full' --gcmode 'archive' --identity '${HOSTNAME}' --ws --wsapi 'admin,debug,eth,miner,net,personal,shh,txpool,web3' --wsorigins '*' --ipcdisable --minerthreads 1 --targetgaslimit 0 --gasprice '1' --unlock '0x${pubkey}' --password /accountpswd.txt" > /geth.sh
+  chmod +x /geth.sh
+fi
+
+echo "starting geth"
+/geth.sh
