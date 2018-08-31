08c3c0716ad43f883aef4485532351ca420423ed
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-08-31 11:47:48 +0200

    Store chaindata outside container

--- run.sh
+++ run.sh
@@ -2,24 +2,23 @@
 set -u
 set -e
 
-if [ ! -e /geth.sh ]
+
+pubkey="88e94a4b7bfc62a38d300d98ce1c09f30fb75e3e"
+
+if [ ! -e /external/datadir ]
 then
   echo "Creating account"
-  pubkey="88e94a4b7bfc62a38d300d98ce1c09f30fb75e3e"
-  mkdir /datadir
-  mkdir /datadir/keystore
-  mv /UTC--2018-06-13T08-21-07.573900500Z--${pubkey} /datadir/keystore/UTC--2018-06-13T08-21-07.573900500Z--${pubkey}
+  mkdir /external/datadir
+  mkdir /external/datadir/keystore
+  mv /UTC--2018-06-13T08-21-07.573900500Z--${pubkey} /external/datadir/keystore/UTC--2018-06-13T08-21-07.573900500Z--${pubkey}
 
   echo "Writing genesis.json"
   sed -e s/##address##/${pubkey}/g /genesis.json.template > /genesis.json
 
   echo "Initializing new blockchain"
-  geth --datadir /datadir init /genesis.json
-
-  echo "Writing start script"
-  echo "exec geth --datadir /datadir --nousb --networkid 2814 --syncmode 'full' --gcmode 'archive' --identity '${HOSTNAME}' --ws --wsapi 'admin,debug,eth,miner,net,personal,shh,txpool,web3' --wsaddr '0.0.0.0' --wsorigins '*' --rpc --rpcaddr '0.0.0.0' --rpcapi 'admin,debug,eth,miner,net,personal,shh,txpool,web3' --ipcdisable --shh --mine --minerthreads 1 --gasprice '1' --targetgaslimit 1203200000 --unlock '0x${pubkey}' --password /accountpswd.txt" > /geth.sh
-  chmod +x /geth.sh
+  geth --datadir /external/datadir init /genesis.json
 fi
 
 echo "starting geth"
-exec /geth.sh
+
+exec geth --datadir /external/datadir --nousb --networkid 2814 --syncmode 'full' --gcmode 'archive' --identity "${HOSTNAME}" --ws --wsapi 'admin,debug,eth,miner,net,personal,shh,txpool,web3' --wsaddr '0.0.0.0' --wsorigins '*' --rpc --rpcaddr '0.0.0.0' --rpcapi 'admin,debug,eth,miner,net,personal,shh,txpool,web3' --ipcdisable --shh --mine --minerthreads 1 --gasprice '1' --targetgaslimit 1203200000 --unlock "0x${pubkey}" --password /accountpswd.txt
